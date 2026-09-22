b262205307fab5b594c3017e971e97695d46fed2
Arhant <Arhantqwe@gmail.com>
2026-09-22 07:12:23 +0530

    m6

--- src/add_student/add_student_component.ts
+++ src/add_student/add_student_component.ts
@@ -1,16 +1,45 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector:"main",
     templateUrl:"add_student_component.html",
-    styleUrl:"add_student_component.scss"
+    styleUrl:"add_student_component.scss",
+    imports: [FormsModule]
 })
+
+
+
 export class MainComponent{
+    id=""
+    rollNo=""
+    name=""
+    fatherName=""
+    gender=""
+    age=""
+    branch=""
+    year=""
+    sem=""
+    mobileNo=""
+    email=""
+    address=""
+    password=""
+
     persons=[
-        {id:1,name:"Raju",age:23},
-        {id:2,name:"Babli",age:21},
-        {id:3,name:"Bunty",age:18},
-        {id:4,name:"Ram",age:20}
-        
+        {
+            id:1,
+            rollNo:101,
+            name:"Raju",
+            fatherName:"RajuKaBaap",
+            gender:"male",
+            age:23,
+            branch:"cs",
+            year:2026,
+            sem:4,
+            mobileNo:1234567899,
+            email:"raju@gmail.com",
+            address:"nagpur",
+            password:1234,
+        },
     ]
 }
