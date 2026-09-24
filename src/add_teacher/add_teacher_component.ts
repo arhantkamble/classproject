52dc6a59b7cb6a607db826e34c6bf91756f1c972
Arhant <Arhantqwe@gmail.com>
2026-09-24 22:57:54 +0530

    m7

---
+++ src/add_teacher/add_teacher_component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: "add_teacher",
+    imports:[FormsModule],
+    standalone:true,
+    styleUrl: "add_teacher_component.scss",
+    templateUrl: "add_teacher_component.html"
+})
+
+export class AddTeacher{
+    
+}
